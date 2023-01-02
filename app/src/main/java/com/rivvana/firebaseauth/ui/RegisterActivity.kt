0048ceaa3b997d0fbbc7a30568c3a2c3e0c4a195
Bagus Rizky M <mohamad.15@students.amikom.ac.id>
2023-01-02 10:50:24 +0700

    .

--- app/src/main/java/com/rivvana/firebaseauth/ui/RegisterActivity.kt
+++ app/src/main/java/com/rivvana/firebaseauth/ui/RegisterActivity.kt
@@ -3,6 +3,7 @@ package com.rivvana.firebaseauth.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
 import com.google.firebase.auth.FirebaseAuth
 import com.rivvana.firebaseauth.R
 import com.rivvana.firebaseauth.databinding.ActivityRegisterBinding
@@ -18,6 +19,31 @@ class RegisterActivity : AppCompatActivity() {
 
         binding.tvLogin.setOnClickListener {
             startActivity(Intent(this, LoginActivity::class.java))
+        }
+
+        binding.btnRegister.setOnClickListener{
+            val email = binding.etEmailRegister.text.toString()
+            val password = binding.etPasswordRegister.text.toString()
+
+            if (email.isEmpty()){
+                binding.etEmailRegister.error = "Masukan Email"
+                binding.etEmailRegister.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                binding.etEmailRegister.error = "Email salah"
+                binding.etEmailRegister.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (password.isEmpty()){
+                binding.etPasswordRegister.error = "Masukan Password"
+                binding.etPasswordRegister.requestFocus()
+                return@setOnClickListener
+            }
+
+
         }
     }
 }
