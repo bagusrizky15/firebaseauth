307433b58c414667837988f560142d5dbeb9858a
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2023-01-11 11:11:35 +0700

    update notification

--- app/src/main/java/com/rivvana/firebaseauth/MessageService.kt
+++ app/src/main/java/com/rivvana/firebaseauth/MessageService.kt
@@ -1,68 +1,13 @@
 package com.rivvana.firebaseauth
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
 import android.content.Intent
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.os.Build
-import android.util.Log
-import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
-import com.google.firebase.messaging.RemoteMessage
-import java.lang.Exception
-import java.net.URL
-
 class MessageService : FirebaseMessagingService() {
-    var intent : Intent? = null
 
-    override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        super.onMessageReceived(remoteMessage)
-        intent = Intent(applicationContext, MainActivity::class.java)
-        intent!!.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-
-        var bmp : Bitmap? = null
-        try {
-            val `in` = URL("https://selectra.in/sites/selectra.in/files/2021-04/mobile-recharge-plans.png").openStream()
-            bmp = BitmapFactory.decodeStream(`in`)
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-
-        val pendingIntent : PendingIntent
-        pendingIntent = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_MUTABLE)
-        } else {
-            PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
-        }
-        val channelId = "Default"
-        val builder = NotificationCompat.Builder(this, channelId)
-            .setSmallIcon(R.mipmap.ic_launcher)
-            .setContentTitle(remoteMessage.data["title"])
-            .setContentText(remoteMessage.data["body"])
-            .setLargeIcon(bmp)
-            .setStyle(NotificationCompat.BigPictureStyle().bigPicture(bmp))
-            .setAutoCancel(true)
-            .setContentIntent(pendingIntent)
-        val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channel = NotificationChannel(
-                channelId,
-                "Default channel",
-                NotificationManager.IMPORTANCE_DEFAULT
-            )
-            manager?.createNotificationChannel(channel)
-        }
-        manager?.notify(0, builder.build())
+    fun generateNotification(title: String, message: String){
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
     }
 
-    override fun onNewToken(token: String) {
-        sendRegistrationToServer(token)
-    }
-
-    private fun sendRegistrationToServer(token: String?) {
-        Log.e("FCM TOKEN", token!!);
-    }
 
 }
