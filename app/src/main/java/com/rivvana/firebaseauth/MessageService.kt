7a35cdd4e5ba72049916735385fc1d1bca029585
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2023-01-11 11:40:45 +0700

    update fcm

--- app/src/main/java/com/rivvana/firebaseauth/MessageService.kt
+++ app/src/main/java/com/rivvana/firebaseauth/MessageService.kt
@@ -1,13 +1,63 @@
 package com.rivvana.firebaseauth
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
+import android.os.Build
+import android.widget.RemoteViews
+import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+const val channelId = "notification_android"
+const val channelName = "package com.rivvana.firebaseauth"
 class MessageService : FirebaseMessagingService() {
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        if (remoteMessage.getNotification() != null){
+            generateNotification(remoteMessage.notification!!.title!!, remoteMessage.notification!!.body!!)
+        }
+    }
+
+    private fun getRemoteView(title: String, message: String): RemoteViews? {
+        val remoteViews = RemoteViews("package com.rivvana.firebaseauth", R.layout.notification)
+
+        remoteViews.setTextViewText(R.id.tv_title, title)
+        remoteViews.setTextViewText(R.id.tv_message, message)
+        remoteViews.setImageViewResource(R.id.app_logo,R.drawable.dummy)
+
+        return remoteViews
+    }
 
     fun generateNotification(title: String, message: String){
         val intent = Intent(this, MainActivity::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
+        //channel id
+        //channel name
+        var builder: NotificationCompat.Builder = NotificationCompat.Builder(applicationContext, channelId)
+            .setSmallIcon(R.drawable.dummy)
+            .setAutoCancel(true)
+            .setVibrate(longArrayOf(1000,1000,1000,1000))
+            .setOnlyAlertOnce(true)
+            .setContentIntent(pendingIntent)
+
+        builder = builder.setContent(getRemoteView(title, message))
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val notificationChannel = NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH)
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+
+        notificationManager.notify(0, builder.build())
+
     }
+
+
 
 
 }
